ffd5e325e67e42a76a1a95a2b8293ab208210427
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-07-30 12:40:44 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(process.env.PORT || 3333,  ()=> console.log('Server running on port 3333'))
+app.listen(process.env.PORT || 3333,  ()=> console.log('Server running on port 3333')) 
